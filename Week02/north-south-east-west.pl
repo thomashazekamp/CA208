ff19fc08e1d1468bf55877e75cb3cac49e898526
thomashazekamp <thomas.hazekamp@gmail.com>
2022-01-19 18:18:59 +0000

    modified file

--- Week02/north-south-east-west.pl
+++ Week02/north-south-east-west.pl
@@ -1,52 +1,35 @@
 north(p, k).
-east(p, q).
-
 north(k, f).
-east(k, l).
-
 north(f, a).
-east(f, g).
-
-east(a, b).
-
 north(q, l).
-east(q, r).
-
 north(l, g).
-east(l, m).
-
 north(g, b).
-east(g, h).
-
-east(b, c).
-
 north(r, m).
-east(r, s).
-
 north(m, h).
-east(m, n).
-
 north(h, c).
-east(h, i).
-
-east(c, d).
-
 north(s, n).
-east(s, t).
-
 north(n, i).
-east(n, o).
-
 north(i, d).
-east(i, j).
-
-east(d, e).
-
 north(t, o).
-
 north(o, j).
-
 north(j, e).
+
+east(p, q).
+east(k, l).
+east(f, g).
+east(a, b).
+east(q, r).
+east(l, m).
+east(g, h).
+east(b, c).
+east(r, s).
+east(m, n).
+east(h, i).
+east(c, d).
+east(s, t).
+east(n, o).
+east(i, j).
+east(d, e).
 
 south(X, Y) :- north(Y, X).
 west(X, Y) :- east(Y, X).
