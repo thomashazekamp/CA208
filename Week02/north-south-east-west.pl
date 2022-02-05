dff07f74364bc7eff67ac1687b8cae406c9d23df
thomashazekamp <thomas.hazekamp@gmail.com>
2022-02-05 17:29:58 +0000

    added rules

--- Week02/north-south-east-west.pl
+++ Week02/north-south-east-west.pl
@@ -35,3 +35,21 @@ east(d, e).
 
 south(X, Y) :- north(Y, X).
 west(X, Y) :- east(Y, X).
+
+due_north(X, Y):- north(X, Y).
+due_north(X, Y):- north(X, Z), due_north(Z, Y).
+
+due_south(X, Y):- south(X, Y).
+due_south(X, Y):- south(X, Z), due_south(Z, Y).
+
+due_west(X, Y):- west(X, Y).
+due_west(X, Y):- west(X, Z), due_west(Z, Y).
+
+due_east(X, Y):- east(X, Y).
+due_east(X, Y):- east(X, Z), due_east(Z, Y).
+
+due_north_west(X, Y):- due_north(X, Z), due_west(Z, Y).
+due_north_east(X, Y):- due_north(X, Z), due_east(Z, Y).
+
+due_south_west(X, Y):- due_south(X, Z), due_west(Z, Y).
+due_south_east(X, Y):- due_south(X, Z), due_east(Z, Y).
