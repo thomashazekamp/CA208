c3f0df0fcde4355e9d12eda0b242db1742a9c16f
thomashazekamp <thomas.hazekamp@gmail.com>
2022-03-24 12:52:40 +0000

    new file and edits

--- Assignment/2-3_Tree.pl
+++ Assignment/2-3_Tree.pl
@@ -1,7 +1,7 @@
 /* This program is for a non-self balancing 2-3 tree */
 
 /* tree(4, tree(3, nil, nil), tree(8, nil, nil)). */
-tree(3, nil, nil).
+test(tree(3, nil, nil, nil, nil)).
 
 /* Base case, if the element is the root */
 in(X, tree(X, _, _)).
@@ -24,6 +24,18 @@ addnode(X, tree(L, Y, R), tree(L, Y, R1)) :- addnode(X, R, R1).
 
 add(X, T1, T2) :- addnode(X, nill, nil), T1 == T2.
 
+/* If the tree is a 2 node tree */
+
+present(Key, tree(Key, _, _)).
+
+present(Key, tree(_, LeftSubTree, _)) :-
+    present(Key, LeftSubTree).
+
+present(Key, tree(_, _, RightSubTree)) :-
+    present(Key, RightSubTree).
+
+/* If the tree is a 3 node tree */
+
 present(LeftKey, tree(LeftKey, _, _, _, _)). % base case
 present(RightKey, tree(_, RightKey, _, _, _)). % base case
 
@@ -36,7 +48,7 @@ present(Key, tree(_, _, _, MidSubTree, _)) :-
 present(Key, tree(_, _, _, _, RightSubTree)) :-
     present(Key, RightSubTree).
 
-% Inserting
+/* Inserting */
 
 inserting(Key, nil, tree(Key, nil, nil)).
 
@@ -48,3 +60,20 @@ inserting(Key, tree(Root, LeftSubTree, RightSubTree), tree(Root, LeftSubTree, Ne
     Key > Root,
     inserting(Key, RightSubTree, NewRightSubTree).
 
+/* Height of a tree */
+
+height(nil, 0).
+
+height(tree(_, L, R), H) :-
+    height(L, LH), height(R, RH), H is max(LH, RH) + 1.
+
+height(tree(_, _, L, M, R), H) :-
+    height(L, LH), height(M, MH), height(R, RH), OneMax is max(LH, MH), H is max(OneMax, RH) + 1.
+
+/* Printing a tree */
+
+prettyPrint(nil, _).
+prettyPrint(tree(Key, LST, RST), D) :- D1 is D + 1, prettyPrint(LST, D1), printKey(Key, D), prettyPrint(RST, D1).
+
+printKey(Key, D) :- D > 0, !, D1 is D - 1, write('\t'), printKey(Key, D1).
+printKey(Key, _) :- write(Key), nl.
